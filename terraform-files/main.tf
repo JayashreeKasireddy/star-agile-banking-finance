7b4816967b9ce3be0731c5f28fe46ca18783c10a
JayashreeKasireddy <jayasrim955@gmail.com>
2024-09-07 18:40:20 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,4 +1,4 @@
-  resource "aws_instance" "app-server" {
+resource "aws_instance" "app-server" {
   ami = ""
   instance_type = "t2.micro"
   key_name = " "
@@ -9,3 +9,16 @@
   private_key = 
   host = self.public_ip
   }
+provisioner "remote-exec" {
+   inline = ["echo 'wait to start the instance' "]
+  }
+tags = {
+  Name = "app-server"
+  }
+provisioner "local-exec" {
+   command = "echo ${aws_instance.app-server.public_ip} > inventory"
+   }
+provisioner "local-exec" {
+   command = "ansible-playbook /var/lib/jenkins/workspace/banking-finance/terraform-files/ansibleplaybook.yml"
+   }
+}
