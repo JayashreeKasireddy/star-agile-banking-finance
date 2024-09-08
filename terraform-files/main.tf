d50d1fd649286c8578b281675e44be11264ea131
JayashreeKasireddy <jayasrim955@gmail.com>
2024-09-08 20:55:08 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -4,21 +4,21 @@ resource "aws_instance" "app-server" {
   key_name = "jaya-key"
   vpc_security_group_ids = ["sg-0ff80cdf8844b2dc4"]
   connection {
-  type = "ssh"
-  user = "ubuntu"
-  private_key= file("./jaya-key.pem")
-  host = self.public_ip
+     type = "ssh"
+     user = "ubuntu"
+     private_key= file("./jaya-key.pem")
+     host = self.public_ip
+     }
+  provisioner "remote-exec" {
+     inline = ["echo 'wait to start the instance' "]
   }
-provisioner "remote-exec" {
-   inline = ["echo 'wait to start the instance' "]
-}
-tags = {
-  Name = "app-server"
+  tags = {
+     Name = "app-server"
+     }
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.app-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/BankingandFinance/terraform-files/ansibleplaybook.yml"
+     }
   }
-provisioner "local-exec" {
-   command = "echo ${aws_instance.app-server.public_ip} > inventory"
-   }
-provisioner "local-exec" {
-   command = "ansible-playbook /var/lib/jenkins/workspace/BankingandFinance/terraform-files/ansibleplaybook.yml"
-   }
-}
