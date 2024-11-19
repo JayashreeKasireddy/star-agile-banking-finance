056e0621ad9d8d6694bc513c4f8f1d03d72474a9
JayashreeKasireddy <jayasrim955@gmail.com>
2024-11-19 19:00:50 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "app-server" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
-  key_name = "jaya-key"
+  key_name = "jayak-key"
   vpc_security_group_ids = ["sg-0ff80cdf8844b2dc4"]
   connection {
      type = "ssh"
      user = "ubuntu"
-     private_key= file("./jaya-key.pem")
+     private_key= file("./jayak-key.pem")
      host = self.public_ip
      }
   provisioner "remote-exec" {
